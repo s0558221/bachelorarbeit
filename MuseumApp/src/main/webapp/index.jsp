0441b5ffd66ad9d76d9d96dbc6ec8be629cf0567
s0558221 <s0558221@htw-berlin.de>
2021-01-03 00:35:53 +0100

    Quiz begonnen

--- MuseumApp/src/main/webapp/index.jsp
+++ MuseumApp/src/main/webapp/index.jsp
@@ -1,4 +1,9 @@
 <html>
+<head>
+    	<!-- <link rel="stylesheet" type="text/css" href="css/style.css" />-->
+	    <script src="js/quizfunctions.js"></script>
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    </head>
 <body>
     <h2>Jersey RESTful Web Application!</h2>
     <p><a href="rest/question">Alle Fragen</a>
@@ -7,5 +12,18 @@
     <p><a href="rest/difficulty">Alle Schwierigkeiten</a>
     <p>Visit <a href="http://jersey.java.net">Project Jersey website</a>
     for more information on Jersey!
+    
+    <div>
+    	<select name="difficulties" id="difficulties-select">
+		</select>
+		<script>
+			getAllDifficulties()
+		</script><br>
+		<select name="topics" id="topics-select">
+		</select>
+		<script>
+			getAllTopics()
+		</script>
+	</div>
 </body>
 </html>
---
+++ MuseumApp/src/main/webapp/js/quizfunctions.js
@@ -0,0 +1,75 @@
+function getAllDifficulties() {
+	
+	let dropdown = document.getElementById('difficulties-select');
+dropdown.length = 0;
+
+let defaultOption = document.createElement('option');
+defaultOption.text = 'Bitte Schwierigkeit wählen';
+
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
+
+const url = 'rest/difficulty';
+
+const request = new XMLHttpRequest();
+request.open('GET', url, true);
+
+request.onload = function() {
+  if (request.status === 200) {
+    const data = JSON.parse(request.responseText);
+    let option;
+    for (let i = 0; i < data.length; i++) {
+      option = document.createElement('option');
+      option.text = data[i].text;
+      option.value = data[i].id;
+      dropdown.add(option);
+    }
+   } else {
+    // Reached the server, but it returned an error
+  }   
+}
+
+request.onerror = function() {
+  console.error('An error occurred fetching the JSON from ' + url);
+};
+
+request.send();
+}
+
+function getAllTopics() {
+	
+	let dropdown = document.getElementById('topics-select');
+dropdown.length = 0;
+
+let defaultOption = document.createElement('option');
+defaultOption.text = 'Bitte Themenbereich wählen';
+
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
+
+const url = 'rest/topic';
+
+const request = new XMLHttpRequest();
+request.open('GET', url, true);
+
+request.onload = function() {
+  if (request.status === 200) {
+    const data = JSON.parse(request.responseText);
+    let option;
+    for (let i = 0; i < data.length; i++) {
+      option = document.createElement('option');
+      option.text = data[i].text;
+      option.value = data[i].id;
+      dropdown.add(option);
+    }
+   } else {
+    // Reached the server, but it returned an error
+  }   
+}
+
+request.onerror = function() {
+  console.error('An error occurred fetching the JSON from ' + url);
+};
+
+request.send();
+}
